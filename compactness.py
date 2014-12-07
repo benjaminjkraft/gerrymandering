62cd5a3d6ef384177c87c64e169914463952a859
Ben Kraft <benjaminjkraft@gmail.com>
2014-12-07 12:40:39 -0500

    Adds population moment

--- compactness.py
+++ compactness.py
@@ -13,10 +13,12 @@
 #  * run on past and state data
 #  * include population data, to compute population moments
 
+import collections
 import csv
 import functools
 import math
 import shapefile
+import os
 
 def get_states(filename='data/state.txt'):
     """Returns a dict of FIPS codes to postal abbreviations."""
@@ -131,4 +133,103 @@ def dump_data(metric, filebase='data/cb_2013_us_cd113_500k',
     with open(out_filename, 'w') as f:
         writer = csv.writer(f)
         writer.writerows(data)
+
+def block_map(state_fips, directory='/tmp/faces/'):
+    all_files = os.listdir(directory)
+    faces_files = [f for f in all_files
+                   if f.startswith('tl_2014_%s' % state_fips)
+                   and f.endswith('_faces.dbf')]
+    if not faces_files:
+        raise ValueError("No files found")
+    b_map = collections.defaultdict(list)
+    for f in faces_files:
+        with open(directory + f, 'rb') as dbf:
+            sf = shapefile.Reader(dbf=dbf)
+            for rec in sf.records():
+                block_code = ''.join(rec[1:4]) + rec[5]
+                cd = rec[30]
+                b_map[block_code].append(cd)
+    cd_map = collections.defaultdict(list)
+    for block_code in b_map:
+        # Sometimes blocks get split; just take the district that most of the
+        # pieces are in.  This isn't perfect but it's a small difference.
+        district_options = collections.Counter(b_map[block_code])
+        cd = district_options.most_common(1)[0][0]
+        cd_map[cd].append(block_code)
+    return cd_map
+
+def block_reader(state_fips, directory='/tmp/faces/'):
+    sf = shapefile.Reader(directory + 'tabblock2010_%s_pophu.shp' % state_fips)
+    districts = enumerate(sf.records())
+    indices_by_block_id = {record[4]: index for index, record in districts}
+    def reader(block_id):
+        if block_id in indices_by_block_id:
+            return sf.shapeRecord(indices_by_block_id[block_id])
+        else:
+            print "block %s not found" % block_id
+            # For some reason, a few blocks don't show up in the population
+            # data.  Let's not worry about it.
+            return None
+    return reader
+
+def population_centroid(block_ids, block_reader):
+    centroid_x = 0
+    centroid_y = 0
+    total_pop = 0
+    for block_id in block_ids:
+        block = block_reader(block_id)
+        if block:
+            pop = block.record[7]
+            block_centroid_x, block_centroid_y = centroid(block.shape)
+            centroid_x += pop * block_centroid_x
+            centroid_y += pop * block_centroid_y
+            total_pop += pop
+    return (centroid_x / total_pop, centroid_y / total_pop)
+
+def population_moment(cd, block_ids, block_reader):
+    moment = 0
+    cd_centroid_x, cd_centroid_y = population_centroid(block_ids, block_reader)
+    total_pop = 0
+    for block_id in block_ids:
+        block = block_reader(block_id)
+        if block:
+            pop = block.record[7]
+            block_centroid_x, block_centroid_y = centroid(block.shape)
+            centroid_dist = ((cd_centroid_x - block_centroid_x) ** 2 +
+                             (cd_centroid_y - block_centroid_y) ** 2)
+            moment += pop * centroid_dist
+            total_pop += pop
+    return area(cd.shape) * total_pop / (moment * 2 * math.pi)
+
+def dump_population_moments(states=None,
+                            filebase='data/tl_2014_us_cd114',
+                            states_filename='data/state.txt',
+                            block_data_directory='/tmp/faces/',
+                            out_filename='data/population_moments.csv'):
+    if states is None:
+        # Filter out Puerto Rico etc. because there's no tabblock data for
+        # them.
+        states = [state for state in get_states().keys() if int(state) < 60]
+    districts = shapefile.Reader(filebase).shapeRecords()
+    # states -> dict of district number -> shapeRecord
+    district_dict = collections.defaultdict(dict)
+    for district in districts:
+        district_dict[district.record[0]][district.record[1]] = district
+    for state in states:
+        data = []
+        print "processing %s" % get_states()[state]
+        bm = block_map(state)
+        br = block_reader(state)
+        for num in district_dict[state]:
+            try:
+                data.append((get_states()[state], num,
+                             population_moment(district_dict[state][num],
+                                               bm[num], br)))
+            except Exception as e:
+                print "%s error in %s-%s" % (e, get_states()[state], num)
+        data.sort()
+        with open(out_filename, 'a') as f:
+            writer = csv.writer(f)
+            writer.writerows(data)
+
 
